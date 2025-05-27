8bd7d8de5ee6d66e85bffa19e860e62ddbf5019a
Yolxanderhc <yolxander.jaca@humancontact.com>
2025-05-27 16:43:39 -0400

    fixed script to empty db before adidng new hote deals part 2

--- scrape_hotels.sh
+++ scrape_hotels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make the HTTP request to our endpoint
-curl -X POST http://localhost:3002/api/scrape-popular-cities \
+curl -X POST http://199.19.72.124:3002/api/scrape-popular-cities \
   -H "Content-Type: application/json" \
   -d '{}'
 
